28c733d4789e3367e93ee581780813a409395017
MaltbyRL <RLMaltby@gmail.com>
2016-02-05 01:36:44 -0800

    ?????

--- public/js/album.js
+++ public/js/album.js
@@ -1,50 +1,19 @@
-"use strict";
-require('dotenv').config();
-var aws = require('aws-sdk');
-var mongoose = require('mongoose');
-var uuid = require('node-uuid');
-var s3 = new aws.S3();
-var fs = require('fs');
+'use strict';
+$(document).ready(init)
 
-
-var Image = mongoose.model('Image' , {
-  filename: String,
-  url: String
-});
-
-mongoose.connect('mongodb://localhost/imageupload', function(err) {
-  if(err) return console.log("err:" , err);
-  var filename = $("#image").val()
-
-  fs.readFile("image", function(err, PIC) {
-    if(err) console.log("err: ", err);
-    var ext = filename.match(/\.\w+$/)[0] || '';
-    var key = uuid.v1() + ext;
-
-    var params = {
-      Bucket: process.env.AWS_BUCKET,
-      Key: filename,
-      Body:  PIC //data buffer
-    };
-
-    s3.putObject(params, function(err, data) {
-      if(err) return console.log("err: ", err);
-      console.log("data: ", data);
-      var url = process.env.AWS_URL + process.env.AWS_BUCKET + '/' + key;
-      console.log("url: ", url);
-
-      var image = new Image({
-        filename: filename,
-        url: url
-      });
-
-      image.save(function() {
-        mongoose.disconnect();
-      })
-
-    });
-
-      console.log("err: ", err);
-      console.log('callback:', PIC)
+function init(){
+  $('form').on('submit', submitPic);
+}
+function submitPic(data){
+  var picTarget = data.delegateTarget.firstElementChild.value
+  console.log("data: ",picTarget)
+  debugger
+  $.post('/users/albumpic', picTarget)
+  .success(function() {
+    location.href = '/login';
+  })
+  .fail(function(err) {
+    alert('Error.  Check console.');
+    console.log('errr:', err);
   });
-});
+}
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,19 @@ var multer = require('multer');
 var upload = multer({
   storage: multer.memoryStorage()
 });
+require('dotenv').config();
+var aws = require('aws-sdk');
+var mongoose = require('mongoose');
+var uuid = require('node-uuid');
+var s3 = new aws.S3();
+var fs = require('fs');
+
+
+var Image = mongoose.model('Image' , {
+  filename: String,
+  url: String
+});
+
 
 var authMiddleware = require('../config/auth');
 
@@ -57,12 +70,6 @@ router.get('/logout', function(req, res, next) {
 router.post('/album', function(req, res) {
   res.send('posted')
 })
-router.post('/albumpic', upload.array('images'), function(req, res) {
-  console.log("req.files:", req.files);
-  // console.log("res:", res);
-  res.render('album')
-})
-
 
 router.post('/changepw', function(req, res, next) {
   console.log('changepw');
@@ -84,6 +91,50 @@ router.post('/changepw', function(req, res, next) {
     }
   });
 });
+
+router.post('/albumpic', upload.array('images'), function(req, res) {
+  console.log("req.files:", req.files);
+  // console.log("res:", res);
+  res.render('album')
+})
+
+mongoose.connect('mongodb://localhost/imageupload', function(err) {
+  if(err) return console.log("err:" , err);
+  var filename = ""
+
+  fs.readFile("image", function(err, PIC) {
+    if(err) console.log("err: ", err);
+    var ext = filename.match(/\.\w+$/)[0] || '';
+    var key = uuid.v1() + ext;
+
+    var params = {
+      Bucket: process.env.AWS_BUCKET,
+      Key: filename,
+      Body:  PIC //data buffer
+    };
+
+    s3.putObject(params, function(err, data) {
+      if(err) return console.log("err: ", err);
+      console.log("data: ", data);
+      var url = process.env.AWS_URL + process.env.AWS_BUCKET + '/' + key;
+      console.log("url: ", url);
+
+      var image = new Image({
+        filename: filename,
+        url: url
+      });
+
+      image.save(function() {
+        mongoose.disconnect();
+      })
+
+    });
+
+      console.log("err: ", err);
+      console.log('callback:', PIC)
+  });
+});
+
 
 module.exports = router;
 
